bccd3d34d0fd0f0c0515de0bed6c485d3f83242f
Prince Anuragi <princanurag07@gmail.com>
2023-02-01 05:12:21 +0530

    Fixed Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,8 @@ import {
   Container,
   IconButton,
   Link,
+  List,
+  ListItem,
   Stack,
   Text,
   useColorMode,
@@ -11,7 +13,7 @@ import {
   useToken,
 } from "@chakra-ui/react";
 import * as React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
 import { GiHighGrass } from "react-icons/gi";
 
 const Footer = () => (
@@ -22,35 +24,70 @@ const Footer = () => (
     py={{ base: "6", md: "8" }}
     px={{ base: "6", md: "8" }}
   >
-    <Stack spacing={{ base: "4", md: "5" }} justify="center" align={"center"}>
-      <Stack justify="center" direction="row" align="center">
-        <ButtonGroup variant="ghost">
-          {/* <IconButton
-            as="a"
-            target={"_blank"}
-            rel="noopener noreferrer"
-            href="https://github.com/orgs/grassroot-uk/dashboard"
-            aria-label="GitHub"
-            icon={<FaGithub fontSize="1.25rem" />}
-          /> */}
-          <IconButton
-            as="a"
-            href="mailto:team@grassroot.uk"
-            aria-label="Email"
-            icon={
-              <GiHighGrass
-                color={useToken("colors", ["brand.300"])[0]}
-                fontSize="1.25rem"
-              />
-            }
-          />
-        </ButtonGroup>
-      </Stack>
-      <Stack>
-        <Text fontSize="sm" color="subtle" textAlign={"center"}>
-          &copy; {new Date().getFullYear()} Grassroot Inc. All rights reserved.
-        </Text>
-      </Stack>
+    <Stack
+      justify="space-between"
+      direction={{ base: "column", md: "row" }}
+      align="center"
+    >
+      <List
+        flexDirection={{ base: "column", md: "row" }}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <ListItem p={"5px"} as={"a"}>
+          About us
+        </ListItem>
+        <ListItem p={"5px"} as={"a"}>
+          Features{" "}
+        </ListItem>
+        <ListItem p={"5px"} as={"a"}>
+          Pricing{" "}
+        </ListItem>
+        <ListItem p={"5px"} as={"a"}>
+          Resources
+        </ListItem>
+      </List>
+      <ButtonGroup variant="ghost">
+        <IconButton
+          as="a"
+          rel="noopener noreferrer"
+          href="#"
+          aria-label="Facebook"
+          icon={<FaFacebook fontSize="1.25rem" />}
+        />
+        <IconButton
+          as="a"
+          rel="noopener noreferrer"
+          href="#"
+          aria-label="Twitter"
+          icon={<FaTwitter fontSize="1.25rem" />}
+        />
+        <IconButton
+          as="a"
+          target={"_blank"}
+          rel="noopener noreferrer"
+          href="https://github.com/orgs/grassroot-uk/dashboard"
+          aria-label="GitHub"
+          icon={<FaGithub fontSize="1.25rem" />}
+        />
+        <IconButton
+          as="a"
+          href="mailto:team@grassroot.uk"
+          aria-label="Email"
+          icon={
+            <GiHighGrass
+              color={useToken("colors", ["brand.300"])[0]}
+              fontSize="1.25rem"
+            />
+          }
+        />
+      </ButtonGroup>
+    </Stack>
+    <Stack>
+      <Text fontSize="sm" color="subtle" textAlign={"center"}>
+        &copy; {new Date().getFullYear()} Grassroot Inc. All rights reserved.
+      </Text>
     </Stack>
   </Box>
 );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 
 import theme from "../theme";
